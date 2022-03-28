0de5ecae32c64e70724d43406bd48b4c16d90c06
Maciek Jablonski <maciekjablonsky@gmail.com>
2022-03-28 23:57:24 +0200

    Added base setup

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake, tools
 class AudioEffectsConanFile(ConanFile):
     name = 'audio-effects'
     settings = 'os', 'compiler', 'arch', 'build_type'
-    generators = 'cmake_paths', 'cmake_find_package'
+    generators = 'cmake_paths','cmake_find_package'
     requires = 'fmt/8.1.1', 'gtest/cci.20210126'
 
     def build(self):
