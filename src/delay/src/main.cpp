81f075b968c8d2c5f9d29200f14c0e6c1e4286be
maciejjablonsky <maciekjablonsky@gmail.com>
2022-07-17 15:32:23 +0200

    Tidying up

--- src/delay/src/main.cpp
+++ src/delay/src/main.cpp
@@ -30,10 +30,28 @@ auto to_kilohertz(std::convertible_to<long double> auto hertz)
     return (long double)hertz / (std::kilo::num / std::kilo::den);
 }
 
-auto to_samples(std::convertible_to<std::chrono::milliseconds> auto period,
+auto to_samples(std::chrono::milliseconds period,
                 auto sample_rate_kHz)
 {
-    return period.count() * sample_rate_kHz;
+    return static_cast<size_t>(period.count() * sample_rate_kHz);
+}
+
+template <typename T> auto apply_delay(std::span<T> samples, auto& delay_line)
+{
+    // append new input to delay buffer
+    std::copy(
+        std::begin(samples), std::end(samples), std::back_inserter(delay_line));
+    // combine new input with delay line
+    std::transform(std::begin(samples),
+                   std::end(samples),
+                   std::begin(delay_line),
+                   std::begin(samples),
+                   [](auto origin, auto delayed) {
+                       constexpr auto feedback_level = 0.6;
+                       constexpr auto output_level   = 1 / 12.f;
+                       return static_cast<T>(
+                           (origin + delayed * feedback_level) * output_level);
+                   });
 }
 
 auto delay_sum(std::convertible_to<std::filesystem::path> auto input_path,
@@ -55,27 +73,13 @@ auto delay_sum(std::convertible_to<std::filesystem::path> auto input_path,
     std::fill_n(
         std::back_inserter(delay_line), delay_length, static_cast<sample_t>(0));
 
-    constexpr auto feedback = 0.6;
-    using amplitude_norm_t  = std::ratio<1, 12>;
-    constexpr auto amplitude_norm =
-        amplitude_norm_t::num * (1.f / amplitude_norm_t::den);
-
     while (!input.eof())
     {
         std::pmr::vector<sample_t> samples_buffer(frame_length);
         auto read_samples = input.read(std::span{samples_buffer});
 
-        std::copy(std::begin(read_samples),
-                  std::end(read_samples),
-                  std::back_inserter(delay_line));
-        std::transform(std::begin(read_samples),
-                       std::end(read_samples),
-                       std::begin(delay_line),
-                       std::begin(read_samples),
-                       [](auto origin, auto delayed) {
-                           return static_cast<sample_t>(
-                               (origin + delayed * feedback) * amplitude_norm);
-                       });
+        apply_delay(read_samples, delay_line);
+
         output.write(std::span{read_samples});
     }
 }
